8a1c8dcfea1d7acf40d12fe92d809591a90845d1
Jayanta Karmakar <jayantakarmakar998@gmail.com>
2024-02-20 00:33:44 +0530

    power of two TODO

---
+++ src/main/java/com/leetcode/solution/leetcodesolutions/easyQuestions/isPowerOfTwo.java
@@ -0,0 +1,21 @@
+package com.leetcode.solution.leetcodesolutions.easyQuestions;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author : jayantakarmakar
+ * @mailto : jayantakarmakar998@mail.com
+ * @created : 20/02/24, Tuesday
+ **/
+
+@Slf4j
+public class isPowerOfTwo {
+    public static boolean powerOfTwo(int n) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        log.info("Output : {}", powerOfTwo(16));
+    }
+}
