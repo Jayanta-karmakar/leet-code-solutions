ae7973555ad07ac414617341f188ff9c1dd01c02
Jayanta Karmakar <jayantakarmakar998@gmail.com>
2025-06-19 09:30:53 +0530

    Refactor nextGreaterPermutation to accept an array and convert it to a list

--- src/main/java/com/leetcode/solution/leetcodesolutions/MediumQuestions/NextPermutation.java
+++ src/main/java/com/leetcode/solution/leetcodesolutions/MediumQuestions/NextPermutation.java
@@ -1,9 +1,5 @@
 package com.leetcode.solution.leetcodesolutions.MediumQuestions;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 /**
  * @author : jayantakarmakar
  * @mailto : jayantakarmakar998@mail.com
@@ -11,13 +7,13 @@ import java.util.List;
  **/
 
 public class NextPermutation {
-    public static List< Integer > nextGreaterPermutation(List< Integer > A) {
-        int n = A.size(); // size of the array.
+    public static int[] nextGreaterPermutation(int[] nums) {
+        int n = nums.length; // size of the array.
 
         // Step 1: Find the break point:
         int ind = -1; // break point
         for (int i = n - 2; i >= 0; i--) {
-            if (A.get(i) < A.get(i + 1)) {
+            if (nums[i] < nums[i + 1]) {
                 // index i is the break point
                 ind = i;
                 break;
@@ -27,36 +23,42 @@ public class NextPermutation {
         // If break point does not exist:
         if (ind == -1) {
             // reverse the whole array:
-            Collections.reverse(A);
-            return A;
+            reverse(nums, 0, n - 1);
+            return nums;
         }
 
         // Step 2: Find the next greater element
         //         and swap it with arr[ind]:
 
         for (int i = n - 1; i > ind; i--) {
-            if (A.get(i) > A.get(ind)) {
-                int tmp = A.get(i);
-                A.set(i, A.get(ind));
-                A.set(ind, tmp);
+            if (nums[i] > nums[ind]) {
+                // Swap
+                int temp = nums[i];
+                nums[i] = nums[ind];
+                nums[ind] = temp;
                 break;
             }
         }
 
         // Step 3: reverse the right half:
-        List<Integer> sublist = A.subList(ind + 1, n);
-        Collections.reverse(sublist);
+        reverse(nums, ind + 1, n - 1);
+        return nums;
+    }
 
-        return A;
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
     }
 
     public static void main(String args[]) {
-        List<Integer> A = Arrays.asList(2, 1, 5, 4, 3, 0, 0);
-        List<Integer> ans = nextGreaterPermutation(A);
+        int[] B = {2, 1, 5, 4, 3, 0, 0};
 
         System.out.print("The next permutation is: [");
-        for (int i = 0; i < ans.size(); i++) {
-            System.out.print(ans.get(i) + " ");
+        for (int i = 0; i < B.length; i++) {
+            System.out.print(B[i] + " ");
         }
         System.out.println("]");
 
