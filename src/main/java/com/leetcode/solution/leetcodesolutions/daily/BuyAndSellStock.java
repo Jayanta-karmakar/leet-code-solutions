ecbff82f300d3e3118815f280aa129798b6bf916
Jayanta Karmakar <jayantakarmakar998@gmail.com>
2025-07-23 09:26:19 +0530

    Add solution for Buy and Sell Stock problem

---
+++ src/main/java/com/leetcode/solution/leetcodesolutions/daily/BuyAndSellStock.java
@@ -0,0 +1,34 @@
+package com.leetcode.solution.leetcodesolutions.daily;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author : jayantakarmakar
+ * @mailto : jayantakarmakar998@mail.com
+ * @created : 23/07/25, Wednesday
+ **/
+
+@Slf4j
+public class BuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE; // Initialize to maximum value
+        int maxProfit = 0; // Initialize to zero
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; // Update minimum price
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice; // Update maximum profit
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        BuyAndSellStock stock = new BuyAndSellStock();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int result = stock.maxProfit(prices);
+        log.info("Maximum profit: {}", result); // Expected output: 5
+    }
+}
